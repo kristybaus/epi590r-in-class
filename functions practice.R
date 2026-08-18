9090c9bd874db2fb151055c2d4b3d3e0e3b6bb10
Kristy Baus <bauskristy082@gmail.com>
2026-08-18 14:04:43 -0400

    functions #1

---
+++ functions practice.R
@@ -0,0 +1,13 @@
+# start out with a number to test
+x <- 3
+
+# you'll want your function to return this number
+x^2
+square <- function(x) {
+	squared_val <- x * x
+	return(squared_val)}
+# test it out
+square(x)
+square(53)
+53^2 # does this match?
+
